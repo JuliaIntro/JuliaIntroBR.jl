506ec5a69f27d942a06d9509cb18a75c715efe72
João <joaoluisok@gmail.com>
2020-06-30 16:23:40 -0300

    translate images

--- src/figures/chap15.jl
+++ src/figures/chap15.jl
@@ -2,7 +2,7 @@ function fig15_1(output::Symbol, font::String, scale::Float64)
   p = TikzPicture(L"""
   \node[anchor=east](blank) at (-2.25, 0) {\tt p};
   \node[draw, fill=mycolor, minimum width=2.5cm, minimum height=1cm](Point) at(0,0){};
-  \node[anchor=west] at (-1.25, 0.75) {\tt Point};
+  \node[anchor=west] at (-1.25, 0.75) {\tt Ponto};
   \node[anchor=east] (x) at(-0.75, 0.25) {\tt x};
   \node[anchor=west] (xv) at (0.25, 0.25) {\tt 3.0};
   \node[anchor=east] (y) at(-0.75, -0.25) {\tt y};
@@ -21,19 +21,19 @@ end
 
 function fig15_2(output::Symbol, font::String, scale::Float64)
   p = TikzPicture(L"""
-  \node[anchor=east](box) at (-2.75, 0) {\tt box};
-  \node[draw, fill=mycolor, minimum width=3.5cm, minimum height=1.5cm](Rectangle) at(0,0){};
-  \node[anchor=west] at (-1.75, 1) {\tt Rectangle};
-  \node[anchor=east] (w) at(-0.5, 0.55) {\tt width};
-  \node[anchor=west] (wv) at (0.5, 0.55) {\tt 100.0};
-  \node[anchor=east] (h) at(-0.5, 0) {\tt height};
-  \node[anchor=west] (hv) at (0.5, 0) {\tt 200.0};
-  \node[anchor=east] (corner) at(-0.5, -0.5) {\tt corner};
+  \node[anchor=east](box) at (-2.75, 0) {\tt caixa};
+  \node[draw, fill=mycolor, minimum width=4.5cm, minimum height=1.5cm](Rectangle) at(0,0){};
+  \node[anchor=west] at (-1.0, 1) {\tt Retângulo};
+  \node[anchor=east] (w) at(0, 0.55) {\tt comprimento};
+  \node[anchor=west] (wv) at (1.0, 0.55) {\tt 100.0};
+  \node[anchor=east] (h) at(0, 0) {\tt altura};
+  \node[anchor=west] (hv) at (1.0, 0) {\tt 200.0};
+  \node[anchor=east] (corner) at(0, -0.5) {\tt vértice};
   \draw[-latex] (box) -- (Rectangle);
   \draw[-latex] (w) -- (wv);
   \draw[-latex] (h) -- (hv);
   \node[draw, fill=mycolor, minimum width=2.5cm, minimum height=1cm](MPoint) at(4,-0.5){};
-  \node[anchor=west] at (2.75, 0.25) {\tt MPoint};
+  \node[anchor=west] at (3.50, 0.25) {\tt MPonto};
   \node[anchor=east] (x) at(3.25, -0.25) {\tt x};
   \node[anchor=west] (xv) at (4.25, -0.25) {\tt 0.0};
   \node[anchor=east] (y) at(3.25, -0.75) {\tt y};
--- src/figures/chap16.jl
+++ src/figures/chap16.jl
@@ -1,13 +1,13 @@
 function fig16_1(output::Symbol, font::String, scale::Float64)
   p = TikzPicture(L"""
-  \node[anchor=east](time) at (-2.5, 0) {\tt time};
-  \node[draw, fill=mycolor, minimum width=3cm, minimum height=1.5cm](MyTime) at(0,0){};
-  \node[anchor=west] at (-1.5, 1) {\tt MyTime};
-  \node[anchor=east] (h) at(-0.25, 0.5) {\tt hour};
+  \node[anchor=east](time) at (-2.5, 0) {\tt tempo};
+  \node[draw, fill=mycolor, minimum width=3.2cm, minimum height=1.5cm](MyTime) at(0,0){};
+  \node[anchor=west] at (-1.5, 1) {\tt minha\_hora};
+  \node[anchor=east] (h) at(-0.25, 0.5) {\tt hora};
   \node[anchor=west] (hv) at (0.75, 0.5) {\tt 11};
-  \node[anchor=east] (m) at(-0.25, 0) {\tt minute};
+  \node[anchor=east] (m) at(-0.25, 0) {\tt minuto};
   \node[anchor=west] (mv) at (0.75, 0) {\tt 59};
-  \node[anchor=east] (s) at(-0.25, -0.5) {\tt second};
+  \node[anchor=east] (s) at(-0.25, -0.5) {\tt segundo};
   \node[anchor=west] (sv) at (0.75, -0.5) {\tt 30};
   \draw[-latex] (time) -- (MyTime);
   \draw[-latex] (h) -- (hv);
